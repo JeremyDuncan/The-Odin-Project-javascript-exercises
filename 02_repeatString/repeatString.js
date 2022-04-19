f8a65a262c82fe2b36251e0142dae35be85a1119
Jeremy Duncan <jeremy.duncan1984@gmail.com>
2022-04-19 19:38:31 -0400

    Fixed repeatString.js so that it passes.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  if (num < 0) {
+    return "ERROR";
+  }
+  
+  repeatResult = "";
+  for (var i = 0; i < num; i++) {
+    repeatResult += string;
+  }
+  return repeatResult;
 };
 
 // Do not edit below this line
